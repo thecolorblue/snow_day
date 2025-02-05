b28d07e5d14a20f2122e88cd27706add839cf56d
Brad Davis <brad@stewardai.com>
2025-02-05 16:31:26 -0500

    add health check

--- src/main.py
+++ src/main.py
@@ -767,5 +767,9 @@ async def read_root(request: Request):
     })
 
 
+@app.get("/")
+def health_check(request: Request):
+    return { "status": "up" }
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
