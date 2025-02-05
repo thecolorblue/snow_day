466bc2dbd9bf1d6c9725e12a2a39f67b596e5443
Brad Davis <brad@stewardai.com>
2025-02-05 16:37:46 -0500

    updated procfile to run main directly

--- src/main.py
+++ src/main.py
@@ -772,4 +772,4 @@ def health_check(request: Request):
     return { "status": "up" }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port= os.getenv('PORT') or 8000)
